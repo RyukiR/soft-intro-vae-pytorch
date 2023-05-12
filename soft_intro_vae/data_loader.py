8ae114628f84e1ef91b565abfbe69cdcf42b72ad
RyukiR <ligengchengucd@163.com>
2023-05-12 09:45:51 +0800

    Small change

--- soft_intro_vae/data_loader.py
+++ soft_intro_vae/data_loader.py
@@ -11,11 +11,11 @@ def load_images_from_folder(folder):
                 images.append(np.array(img))
     return images
 
-folder = "D:\\GitHub\\T2I-Adapter\\preprocessed_outputs\\trn_stim_data-sketch"
+folder = "D:\\GitHub\\T2I-Adapter\\preprocessed_outputs\\trn_stim_data-depth"
 images = load_images_from_folder(folder)
 
 # 将图片列表转化为numpy数组
 images_np = np.array(images)
 
 # 保存numpy数组到np文件
-np.save('D:\\GitHub\\soft-intro-vae-pytorch\\soft_intro_vae\\data_preprocessor\\trn_stim_data-sketch.npy', images_np)
+np.save('D:\\GitHub\\soft-intro-vae-pytorch\\soft_intro_vae\\data_preprocessor\\trn_stim_data-depth.npy', images_np)
